e9440e978c670ecc83d4df1b5068470d15889220
Nikolay Valasatau <mikalai.valasatau@mapbox.com>
2020-10-15 10:40:45 +0300

    Fix MBXPeerWrapper duplication

--- MapboxCoreNavigation/MBXPeerWrapper.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface MBXPeerWrapper
-
-@end
--- MapboxCoreNavigation/SkuTokenProvider.swift
+++ MapboxCoreNavigation/SkuTokenProvider.swift
@@ -1,6 +1,7 @@
 import Foundation
 import MapboxDirections
 import MapboxNavigationNative
+import MapboxCommon
 
 class SkuTokenProvider: SkuTokenSource {
     var peer: MBXPeerWrapper?
